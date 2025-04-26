9119b56f9728927a23f0ad241cc0f29d7df51328
Rowell2002 <chethanarowell2002@gmail.com>
2025-04-26 13:39:10 +0530

    Improved usability

--- movierating/src/main/java/com/example/movierating/model/Genre.java
+++ movierating/src/main/java/com/example/movierating/model/Genre.java
@@ -1,9 +1,22 @@
 package com.example.movierating.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Genre {
     ACTION,
     COMEDY,
     HORROR,
     ROMANTIC,
-    SCIENCE_FICTION
+    SCIENCE_FICTION;
+
+    @JsonCreator
+    public static Genre fromString(String genre) {
+        return Genre.valueOf(genre.trim().toUpperCase());
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.name();
+    }
 }
